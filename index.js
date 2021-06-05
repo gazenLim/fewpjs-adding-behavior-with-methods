c76b1fa172bd875de35ec29be1e49c276928bf9e
gajen <meetgajen@yahoo.com>
2021-06-05 15:14:08 -0400

    commit tree d482de8f0a7c8586ad453540cb031399e014787d

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+
+
+class Cat {
+    constructor(name,gender){
+    this.name = name;
+    this.gender = gender
+}
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+
+class Dog {
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+    
+}
+
+
+class Bird {
+  
+
+    constructor(name,gender){
+        this.name =name;
+        this.gender = gender
+    }
+    
+    speak(bird){
+        return `It's me! ${this.name}, the parrot!`
+    }
+    
+    speak(bird2){
+        return `${this.name}, squawk!`
+    }
+   
+}
+
+
